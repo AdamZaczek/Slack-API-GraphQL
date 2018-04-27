7915cadd200ca3849620634bdca78d293dbc84e9
Andreas Hubel <andi@saerdnaer.de>
2018-04-27 19:24:53 +0200

    add allChannels

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,6 +6,7 @@ import {
 import fetch from 'node-fetch'
 
 import Im from './ImType'
+import Channel from './channelType'
 
 const Query = new GraphQLObjectType({
   name: 'SlackAPI',
@@ -18,6 +19,15 @@ const Query = new GraphQLObjectType({
         return fetch(`https://slack.com/api/im.list?token=${slackToken}&pretty=1`)
           .then(res => res.json())
           .then(res => res.ims)
+      }
+    },
+    allChannels: {
+      type: new GraphQLList(Channel),
+      description: 'Returns list of channels',
+      resolve: (root, args, { slackToken }) => {
+        return fetch(`https://slack.com/api/channels.list?token=${slackToken}&pretty=1`)
+          .then(res => res.json())
+          .then(res => res.channels)
       }
     }
   })
